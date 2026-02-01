64dbf1f78c5b9527187758c73766b7ebd6e76c70
RonDevHub <no_reply@rondev.de>
2026-02-01 10:03:13 +0000

    SÄCHSISCH HARDCORE VERSION
    SÄCHSISCH HARDCORE VERSION

---
+++ lang/dialekt3.php
@@ -0,0 +1,187 @@
+<?php
+return [
+    // GitHub
+    // Allgmein
+    'stars'                     => 'Schdärnchen',
+    'forks'                     => 'Forks',
+    'issues'                    => 'Probleemchen',
+    'watchers'                  => 'Gugger',
+    'release'                   => 'Rilies',
+    'license'                   => 'Lizänz',
+    'top_language'              => 'Hauptschbroche',
+
+    // Metriken (Alle Repos)
+    'stars_all'                 => 'Schdärnchen (Alle)',
+    'forks_all'                 => 'Forks (Alle)',
+    'issues_all'                => 'Probleemchen (Alle)',
+    'watchers_all'              => 'Gugger (Alle)',
+    'downloads_all'             => 'Runtergeladne (Alle)',
+    'branches_all'              => 'Zweische (Alle)',
+    'size_all'                  => 'Größe (Alle)',
+    'top_languages_all'         => 'Hauptschbrochn (Alle)',
+    'pull_requests_all'         => 'Zieh-Anfroochn (Alle)',
+    'prs_merged_all'            => 'Zammjeklatschte PRs (Alle)',
+    'subscribers_count_all'     => 'Abonnännten (Alle)',
+    'success_rate_all'          => 'Erfolschquote (Alle)',
+    'files_all'                 => 'Dateien (Alle)',
+    'tags_all'                  => 'Zeddel (Alle)',
+    'projects_all'              => 'Brojekde (Alle)',
+    'releases_all'              => 'Rilies (Alle)',
+
+    // Metriken (Einzelnes Repo)
+    'lines'                     => 'Zeilchen',
+    'lines_added'               => 'Dazu jemorscht',
+    'lines_deleted'             => 'Wegjeschmissn',
+    'milestones'                => 'Meilnschdeene',
+    'milestones_open'           => 'Offne Meilnschdeene',
+    'milestones_closed'         => 'Zugemachte Meilnschdeene',
+    'milestones_all'            => 'Meilnschdeene (Alle)',
+    'milestones_allopen'        => 'Meilnschdeene (Alle offn)',
+    'milestones_allclosed'      => 'Meilnschdeene (Alle zu)',
+    'downloads'                 => 'Runterjeladne',
+    'downloads_latest'          => 'Runterjeladne (Letzdes Rilies)',
+    'branches'                  => 'Zweische',
+    'size'                      => 'Größe',
+    'created_at'                => 'Jebastelt am',
+    'pull_requests'             => 'Zieh-Anfroochn',
+    'prs_merged'                => 'Zammjeklatschte PRs',
+    'subscribers_count'         => 'Abonnännten',
+    'success_rate'              => 'Erfolschquote',
+    'files'                     => 'Dateien',
+    'tags'                      => 'Zeddel',
+    'projects'                  => 'Brojekde',
+    'releases'                  => 'Rilies',
+
+    // Push & Commit
+    'push_date'                 => 'Letzder Schuppser',
+    'push_time'                 => 'Letzder Schuppser',
+    'push_datetime'             => 'Letzder Schuppser',
+    'push_lines'                => 'Zeilchen',
+    'push_info'                 => 'Info zum letzdn Schuppser',
+    'commit_date'               => 'Letzder Kommid',
+    'commit_time'               => 'Letzder Kommid',
+    'commit_datetime'           => 'Letzder Kommid',
+    'commit_lines'              => 'Kommid-Zeilchen',
+    'commit_info'               => 'Info zum letzdn Kommid',
+
+    // Benutzer-Metriken
+    'repos_count'               => 'Öffentliche Rebosidoris',
+    'follower'                  => 'Die dir hinnerher dappeln',
+    'follower_name'             => 'Letzder Hinnerherdappeler',
+    'following'                 => 'Die du hinnerher dappelst',
+    'following_name'            => 'Letzder, dem du hinnerher bist',
+
+    // Gists
+    'gists'                     => 'Jisstsche',
+    'gist_not_found'            => 'Des Jisstsche jibds nich',
+    'gist_size'                 => 'Jisstsche-Größe',
+    'gist_forks'                => 'Jisstsche-Forks',
+    'gists_list_short'          => 'Letzde Jisstsche',
+    'gists_listall'             => 'Alle Jisstsche',
+    'gists_size'                => 'Janzgröße',
+    'gists_date'                => 'Letzdes Jisstsche',
+    'gists_forks'               => 'Jisstsche-Forks',
+
+    // Versionsprüfung
+    'version_check'             => 'Version nachguggn',
+    'no_version_found'          => 'Keene Version anjejeben',
+    'new_release_available'     => 'Neies Rilies vorhande:',
+    'check_release'             => 'Rilies nachguggn',
+    'check_ok'                  => 'Passt scho',
+    'error'                     => 'Fehla',
+
+    // Benutzer-Infos (Cases)
+    'name_label'                => 'Nu gudn Tach, ich bin der',
+    'company_label'             => 'Firma',
+    'no_company'                => 'Nischd ejetragen',
+    'location_label'            => 'Ich komm aus',
+    'no_location'               => 'Irjendwo im sächs’schen Nischd 🌌',
+    'status_label'              => 'Zuschdand',
+    'no_status'                 => 'Keenen Zuschdand ejestellt',
+    'account_created'           => 'Kondo jemorscht',
+    'last_updated'              => 'Zuletzt rumjefummelt',
+    'time_year_singular'        => 'Jahrchen',
+    'time_year_plural'          => 'Jahrchen',
+    'time_month_singular'       => 'Monadschen',
+    'time_month_plural'         => 'Monadschen',
+    'time_day_singular'         => 'Dachschen',
+    'time_day_plural'           => 'Dachschen',
+
+    // Commit/Push Detail-Felder (Cases)
+    'commits_repo'              => 'Kommidde',
+    'commits_all'               => 'Kommidde (Alle)',
+    'commits_last'              => 'Letzder Kommid (Datum)',
+    'commits_last_info'         => 'Letzder Kommid (Info)',
+
+    // Diskussionen (Cases)
+    'discussions_count'         => 'Jebrabbel',
+    'discussions_lastdate'      => 'Letzdes Jebrabbel (Datum)',
+    'discussions_lastupdate'    => 'Letzdes Jebrabbel (Update)',
+    'discussions_lasttitle'     => 'Letzder Jebrabbel-Tidel',
+    'discussions_lastauthor'    => 'Letzder Jebrabbel-Schreiber',
+
+    // Codeberg
+    // Allgemein / Basics
+    'name'                          => 'Name',
+    'language'                      => 'Schbroche',
+    'updated_at'                    => 'Zuletzt rumjefummelt',
+    'branch_default'                => 'Schdandard-Zweig',
+
+    // Issues (Einzel + Alle Repos)
+    'issues_open'                   => 'Offne Probleemchen',
+    'issues_closed'                 => 'Zugemachte Probleemchen',
+    'issues_total'                  => 'Alle Probleemchen',
+    'issues_allopen'                => 'Offne Probleemchen (Alle)',
+    'issues_all'                    => 'Probleemchen (Alle)',
+    'issues_allclosed'              => 'Zugemachte Probleemchen (Alle)',
+
+    // Pull Requests (Einzel + Alle Repos)
+    'prs_total'                     => 'Alle Zieh-Anfroochn',
+    'prs_open'                      => 'Offne Zieh-Anfroochn',
+    'prs_closed'                    => 'Zugemachte Zieh-Anfroochn',
+    'prs_all'                       => 'Zieh-Anfroochn (Alle)',
+    'prs_allopen'                   => 'Offne Zieh-Anfroochn (Alle)',
+    'prs_allclosed'                 => 'Zugemachte Zieh-Anfroochn (Alle)',
+
+    // Releases & Versionen
+    'release_label'                 => 'Letzdes Rilies',
+    'release_tag_label'             => 'Neisdes Zeddel',
+    'release_check_label'           => 'Rilies-Nachgugg',
+    'release_new_label'             => 'Neies Rilies do',
+    'releases_label'                => 'Rilies j’samt',
+
+    // Benutzer / User
+    'name_label'                    => 'Nu gudn Tach, ich bin der',
+    'location_label'                => 'Ich komm aus',
+    'no_location'                   => 'Irjendwo im sächs’schen Nischd 🌌',
+    'no_company'                    => 'Nischd ejetragen',
+    'created'                       => 'Jebastelt',
+    'followers'                     => 'Hinnerherdappeler',
+    'following'                     => 'Hintnachdappeln',
+    'stars_give'                    => 'Vergebne Schdärnchen',
+    'repos'                         => 'Öffentliche Rebosidoris',
+    'register_on'                   => 'Anjemeld am',
+    'register_since'                => 'Anjemeld seit',
+
+    // Commits
+    'last_commit'                   => 'Letzder Kommid',
+
+    // Milestones
+    'milestones_info'               => 'Meilnschdeen-Info',
+    'milestones_info_open'          => 'Offne Meilnschdeen (Info)',
+    'milestones_info_closed'        => 'Zugemachte Meilnschdeen (Info)',
+    'milestones_all_open'           => 'Meilnschdeen (Alle offn)',
+    'milestones_all_closed'         => 'Meilnschdeen (Alle zu)',
+
+    // Downloads
+    'downloads'                     => 'Runterjeladne',
+    'downloads_latest'              => 'Runterjeladne (Letzdes Rilies)',
+    'downloads_all'                 => 'Runterjeladne (Alle)',
+
+    // Zusätzliche Labels für „all“-Summen
+    'stars_all'                     => 'Schdärnchen (Alle)',
+    'watchers_all'                  => 'Gugger (Alle)',
+    'forks_all'                     => 'Forks (Alle)',
+    'size_all'                      => 'Größe (Alle)',
+    'releases_all'                  => 'Rilies (Alle)',
+];
