c3eca1fb502b393cc7c551b035c9dae565cda0fe
RonDevHub <no_reply@rondev.de>
2026-02-01 10:21:17 +0000

    FRÄNKISCH HARDCORE VERSION
    FRÄNKISCH HARDCORE VERSION

---
+++ lang/dialekt4.php
@@ -0,0 +1,187 @@
+<?php
+return [
+    // GitHub
+    // Allgmein
+    'stars'                     => 'Schdärdla',
+    'forks'                     => 'Forks',
+    'issues'                    => 'Gschmarri',
+    'watchers'                  => 'Guggerla',
+    'release'                   => 'Riliesla',
+    'license'                   => 'Lizänz',
+    'top_language'              => 'Häidschbrooch',
+
+    // Metriken (Alle Repos)
+    'stars_all'                 => 'Schdärdla (Allä)',
+    'forks_all'                 => 'Forks (Allä)',
+    'issues_all'                => 'Gschmarri (Allä)',
+    'watchers_all'              => 'Guggerla (Allä)',
+    'downloads_all'             => 'Runnerg’ladd (Allä)',
+    'branches_all'              => 'Zwäigla (Allä)',
+    'size_all'                  => 'Größn (Allä)',
+    'top_languages_all'         => 'Häidschbroochä (Allä)',
+    'pull_requests_all'         => 'Zieh‑Affroochä (Allä)',
+    'prs_merged_all'            => 'Zammg’würschdelde PRs',
+    'subscribers_count_all'     => 'Abonnäntla (Allä)',
+    'success_rate_all'          => 'Erfolgsquodd (Allä)',
+    'files_all'                 => 'Dateilä (Allä)',
+    'tags_all'                  => 'Zeddla (Allä)',
+    'projects_all'              => 'Brochdla (Allä)',
+    'releases_all'              => 'Riliesla (Allä)',
+
+    // Metriken (Einzelnes Repo)
+    'lines'                     => 'Zeilnla',
+    'lines_added'               => 'Dazug’schmierd',
+    'lines_deleted'             => 'Wegg’rammd',
+    'milestones'                => 'Meilnschdeinä',
+    'milestones_open'           => 'Offne Meilnschdeinä',
+    'milestones_closed'         => 'Zug’machdä Meilnschdeinä',
+    'milestones_all'            => 'Meilnschdeinä (Allä)',
+    'milestones_allopen'        => 'Meilnschdeinä (Allä offn)',
+    'milestones_allclosed'      => 'Meilnschdeinä (Allä zua)',
+    'downloads'                 => 'Runnerg’ladd',
+    'downloads_latest'          => 'Runnerg’ladd (Letzds Riliesla)',
+    'branches'                  => 'Zwäigla',
+    'size'                      => 'Größn',
+    'created_at'                => 'Zammg’bastld am',
+    'pull_requests'             => 'Zieh‑Affroochä',
+    'prs_merged'                => 'Zammg’würschdelde PRs',
+    'subscribers_count'         => 'Abonnäntla',
+    'success_rate'              => 'Erfolgsquodd',
+    'files'                     => 'Dateilä',
+    'tags'                      => 'Zeddla',
+    'projects'                  => 'Brochdla',
+    'releases'                  => 'Riliesla',
+
+    // Push & Commit
+    'push_date'                 => 'Letzder Schubbser',
+    'push_time'                 => 'Letzder Schubbser',
+    'push_datetime'             => 'Letzder Schubbser',
+    'push_lines'                => 'Zeilnla',
+    'push_info'                 => 'Gschichd vom letzdn Schubbser',
+    'commit_date'               => 'Letzder Gschmier',
+    'commit_time'               => 'Letzder Gschmier',
+    'commit_datetime'           => 'Letzder Gschmier',
+    'commit_lines'              => 'Gschmier‑Zeiln',
+    'commit_info'               => 'Gschichd vom letzdn Gschmier',
+
+    // Benutzer-Metriken
+    'repos_count'               => 'Öffendliche Rebosidoris',
+    'follower'                  => 'Die dir hinnerher dappeln',
+    'follower_name'             => 'Letzder Hinnerher‑Dappeler',
+    'following'                 => 'Die du hinnerher dappelst',
+    'following_name'            => 'Letzder, demm du hinnerher bist',
+
+    // Gists
+    'gists'                     => 'Gschmarri‑Schnipsl',
+    'gist_not_found'            => 'Des Gschmarri gibbd’s ned',
+    'gist_size'                 => 'Gschmarri‑Größn',
+    'gist_forks'                => 'Gschmarri‑Forks',
+    'gists_list_short'          => 'Letzde Gschmarri',
+    'gists_listall'             => 'Allä Gschmarri',
+    'gists_size'                => 'G’samdggrößn',
+    'gists_date'                => 'Letzds Gschmarri',
+    'gists_forks'               => 'Gschmarri‑Forks',
+
+    // Versionsprüfung
+    'version_check'             => 'Version nachguggn',
+    'no_version_found'          => 'Käi Version aageem',
+    'new_release_available'     => 'A neis Riliesla is do:',
+    'check_release'             => 'Riliesla nachguggn',
+    'check_ok'                  => 'Bassd scho',
+    'error'                     => 'Fehla',
+
+    // Benutzer-Infos (Cases)
+    'name_label'                => 'Servas, i bin da',
+    'company_label'             => 'Firma',
+    'no_company'                => 'Käi Angab',
+    'location_label'            => 'I kumm aus',
+    'no_location'               => 'Irgendwo im fränggischn Nischd 🌌',
+    'status_label'              => 'Zuschdand',
+    'no_status'                 => 'Käi Zuschdand eigschdelld',
+    'account_created'           => 'Kondo zammg’bastld',
+    'last_updated'              => 'Zuletzt rumg’fummld',
+    'time_year_singular'        => 'Jahrla',
+    'time_year_plural'          => 'Jahrla',
+    'time_month_singular'       => 'Monadsla',
+    'time_month_plural'         => 'Monadsla',
+    'time_day_singular'         => 'Dächla',
+    'time_day_plural'           => 'Dächla',
+
+    // Commit/Push Detail-Felder (Cases)
+    'commits_repo'              => 'Gschmier',
+    'commits_all'               => 'Gschmier (Allä)',
+    'commits_last'              => 'Letzder Gschmier (Datum)',
+    'commits_last_info'         => 'Letzder Gschmier (Gschichd)',
+
+    // Diskussionen (Cases)
+    'discussions_count'         => 'Gebabbel',
+    'discussions_lastdate'      => 'Letzds Gebabbel (Datum)',
+    'discussions_lastupdate'    => 'Letzds Gebabbel (Update)',
+    'discussions_lasttitle'     => 'Letzder Gebabbel‑Titel',
+    'discussions_lastauthor'    => 'Letzder Gebabbel‑Schreiba',
+
+    // Codeberg
+    // Allgemein / Basics
+    'name'                          => 'Nama',
+    'language'                      => 'Schbrooch',
+    'updated_at'                    => 'Zuletzt rumg’fummld',
+    'branch_default'                => 'Schdandard‑Zwäig',
+
+    // Issues (Einzel + Alle Repos)
+    'issues_open'                   => 'Offne Gschmarri',
+    'issues_closed'                 => 'Zug’machdä Gschmarri',
+    'issues_total'                  => 'Allä Gschmarri',
+    'issues_allopen'                => 'Offne Gschmarri (Allä)',
+    'issues_all'                    => 'Gschmarri (Allä)',
+    'issues_allclosed'              => 'Zug’machdä Gschmarri (Allä)',
+
+    // Pull Requests (Einzel + Alle Repos)
+    'prs_total'                     => 'Allä Zieh‑Affroochä',
+    'prs_open'                      => 'Offne Zieh‑Affroochä',
+    'prs_closed'                    => 'Zug’machdä Zieh‑Affroochä',
+    'prs_all'                       => 'Zieh‑Affroochä (Allä)',
+    'prs_allopen'                   => 'Offne Zieh‑Affroochä (Allä)',
+    'prs_allclosed'                 => 'Zug’machdä Zieh‑Affroochä (Allä)',
+
+    // Releases & Versionen
+    'release_label'                 => 'Letzds Riliesla',
+    'release_tag_label'             => 'Neisds Zeddla',
+    'release_check_label'           => 'Rilies‑Nachgugg',
+    'release_new_label'             => 'Neis Riliesla do',
+    'releases_label'                => 'Riliesla g’samdd',
+
+    // Benutzer / User
+    'name_label'                    => 'Servas, i bin da',
+    'location_label'                => 'I kumm aus',
+    'no_location'                   => 'Irgendwo im fränggischn Nischd 🌌',
+    'no_company'                    => 'Käi Angab',
+    'created'                       => 'Zammg’bastld',
+    'followers'                     => 'Hinnerher‑Dappeler',
+    'following'                     => 'Hintnachdappeln',
+    'stars_give'                    => 'Vergebnä Schdärdla',
+    'repos'                         => 'Öffendliche Rebosidoris',
+    'register_on'                   => 'Aagemeld am',
+    'register_since'                => 'Aagemeld seit',
+
+    // Commits
+    'last_commit'                   => 'Letzder Gschmier',
+
+    // Milestones
+    'milestones_info'               => 'Meilnschdeen‑Gschichd',
+    'milestones_info_open'          => 'Offne Meilnschdeen (Gschichd)',
+    'milestones_info_closed'        => 'Zug’machdä Meilnschdeen (Gschichd)',
+    'milestones_all_open'           => 'Meilnschdeen (Allä offn)',
+    'milestones_all_closed'         => 'Meilnschdeen (Allä zua)',
+
+    // Downloads
+    'downloads'                     => 'Runnerg’ladd',
+    'downloads_latest'              => 'Runnerg’ladd (Letzds Riliesla)',
+    'downloads_all'                 => 'Runnerg’ladd (Allä)',
+
+    // Zusätzliche Labels für „all“-Summen
+    'stars_all'                     => 'Schdärdla (Allä)',
+    'watchers_all'                  => 'Guggerla (Allä)',
+    'forks_all'                     => 'Forks (Allä)',
+    'size_all'                      => 'Größn (Allä)',
+    'releases_all'                  => 'Riliesla (Allä)',
+];
