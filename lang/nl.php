2977b65ff3e67825f62b805596eeb58d62caf9e3
RonDevHub <no_reply@rondev.de>
2026-02-01 07:57:11 +0000

    lang/nl.php hinzugefügt

---
+++ lang/nl.php
@@ -0,0 +1,187 @@
+<?php
+return [
+    // GitHub
+    // Algemeen
+    'stars'                     => 'Sterren',
+    'forks'                     => 'Forks',
+    'issues'                    => 'Issues',
+    'watchers'                  => 'Volgers',
+    'release'                   => 'Release',
+    'license'                   => 'Licentie',
+    'top_language'              => 'Toptaal',
+
+    // Statistieken (Alle Repos)
+    'stars_all'                 => 'Sterren (Alle)',
+    'forks_all'                 => 'Forks (Alle)',
+    'issues_all'                => 'Issues (Alle)',
+    'watchers_all'              => 'Volgers (Alle)',
+    'downloads_all'             => 'Downloads (Alle)',
+    'branches_all'              => 'Branches (Alle)',
+    'size_all'                  => 'Grootte (Alle)',
+    'top_languages_all'         => 'Toptalen (Alle)',
+    'pull_requests_all'         => 'PR’s (Alle)',
+    'prs_merged_all'            => 'Samengevoegde PR’s (Alle)',
+    'subscribers_count_all'     => 'Abonnees (Alle)',
+    'success_rate_all'          => 'Succesratio (Alle)',
+    'files_all'                 => 'Bestanden (Alle)',
+    'tags_all'                  => 'Tags (Alle)',
+    'projects_all'              => 'Projecten (Alle)',
+    'releases_all'              => 'Releases (Alle)',
+
+    // Statistieken (Enkel Repo)
+    'lines'                     => 'Regels',
+    'lines_added'               => 'Regels toegevoegd',
+    'lines_deleted'             => 'Regels verwijderd',
+    'milestones'                => 'Mijlpalen',
+    'milestones_open'           => 'Mijlpalen (Open)',
+    'milestones_closed'         => 'Mijlpalen (Gesloten)',
+    'milestones_all'            => 'Mijlpalen (Alle)',
+    'milestones_allopen'        => 'Mijlpalen (Alle Open)',
+    'milestones_allclosed'      => 'Mijlpalen (Alle Gesloten)',
+    'downloads'                 => 'Downloads',
+    'downloads_latest'          => 'Downloads Laatste Release',
+    'branches'                  => 'Branches',
+    'size'                      => 'Grootte',
+    'created_at'                => 'Aangemaakt',
+    'pull_requests'             => 'Pull-Requests',
+    'prs_merged'                => 'Samengevoegde PR’s',
+    'subscribers_count'         => 'Abonnees',
+    'success_rate'              => 'Succesratio',
+    'files'                     => 'Bestanden',
+    'tags'                      => 'Tags',
+    'projects'                  => 'Projecten',
+    'releases'                  => 'Releases',
+
+    // Push & Commit
+    'push_date'                 => 'Laatste Push',
+    'push_time'                 => 'Laatste Push',
+    'push_datetime'             => 'Laatste Push',
+    'push_lines'                => 'Regels',
+    'push_info'                 => 'Laatste Push Info',
+    'commit_date'               => 'Laatste Commit',
+    'commit_time'               => 'Laatste Commit',
+    'commit_datetime'           => 'Laatste Commit',
+    'commit_lines'              => 'Laatste Commit',
+    'commit_info'               => 'Laatste Commit Info',
+
+    // Gebruikersstatistieken
+    'repos_count'               => 'Openbare Repos',
+    'follower'                  => 'Volgers',
+    'follower_name'             => 'Laatste Volger',
+    'following'                 => 'Volgend',
+    'following_name'            => 'Laatste Following',
+
+    // Gists
+    'gists'                     => 'Gists',
+    'gist_not_found'            => 'Gist niet gevonden',
+    'gist_size'                 => 'Gistgrootte',
+    'gist_forks'                => 'Gist Forks',
+    'gists_list_short'          => 'Laatste Gists',
+    'gists_listall'             => 'Alle Gists',
+    'gists_size'                => 'Totale grootte',
+    'gists_date'                => 'Laatste Gist',
+    'gists_forks'               => 'Gist Forks',
+
+    // Versiecontrole
+    'version_check'             => 'Versiecontrole',
+    'no_version_found'          => 'Geen versie opgegeven',
+    'new_release_available'     => 'Nieuwe versie beschikbaar:',
+    'check_release'             => 'Release controleren',
+    'check_ok'                  => 'OK',
+    'error'                     => 'Fout',
+
+    // Gebruikersinfo (Cases)
+    'name_label'                => 'Hallo, mijn naam is',
+    'company_label'             => 'Bedrijf',
+    'no_company'                => 'Geen vermelding',
+    'location_label'            => 'Ik kom uit',
+    'no_location'               => 'Ergens in de Matrix 🌌',
+    'status_label'              => 'Status',
+    'no_status'                 => 'Geen status ingesteld',
+    'account_created'           => 'Account aangemaakt',
+    'last_updated'              => 'Laatst bijgewerkt',
+    'time_year_singular'        => 'Jaar',
+    'time_year_plural'          => 'Jaren',
+    'time_month_singular'       => 'Maand',
+    'time_month_plural'         => 'Maanden',
+    'time_day_singular'         => 'Dag',
+    'time_day_plural'           => 'Dagen',
+
+    // Commit/Push Details (Cases)
+    'commits_repo'              => 'Commits',
+    'commits_all'               => 'Commits (Alle)',
+    'commits_last'              => 'Laatste Commit (Datum)',
+    'commits_last_info'         => 'Laatste Commit (Info)',
+
+    // Discussies (Cases)
+    'discussions_count'         => 'Discussies',
+    'discussions_lastdate'      => 'Laatste discussie (Datum)',
+    'discussions_lastupdate'    => 'Laatste discussie (Update)',
+    'discussions_lasttitle'     => 'Laatste discussietitel',
+    'discussions_lastauthor'    => 'Laatste discussie-auteur',
+
+    // Codeberg
+    // Algemeen / Basis
+    'name'                          => 'Naam',
+    'language'                      => 'Taal',
+    'updated_at'                    => 'Laatste update',
+    'branch_default'                => 'Standaardbranch',
+
+    // Issues (Enkel + Alle Repos)
+    'issues_open'                   => 'Open Issues',
+    'issues_closed'                 => 'Gesloten Issues',
+    'issues_total'                  => 'Alle Issues',
+    'issues_allopen'                => 'Open Issues (Alle Repos)',
+    'issues_all'                    => 'Issues (Alle Repos)',
+    'issues_allclosed'              => 'Gesloten Issues (Alle Repos)',
+
+    // Pull Requests (Enkel + Alle Repos)
+    'prs_total'                     => 'Alle PR’s',
+    'prs_open'                      => 'Open PR’s',
+    'prs_closed'                    => 'Gesloten PR’s',
+    'prs_all'                       => 'PR’s (Alle Repos)',
+    'prs_allopen'                   => 'Open PR’s (Alle Repos)',
+    'prs_allclosed'                 => 'Gesloten PR’s (Alle Repos)',
+
+    // Releases & Versies
+    'release_label'                 => 'Laatste Release',
+    'release_tag_label'             => 'Nieuwste Tag',
+    'release_check_label'           => 'Releasecontrole',
+    'release_new_label'             => 'Nieuwe Release beschikbaar',
+    'releases_label'                => 'Totaal Releases',
+
+    // Gebruiker
+    'name_label'                    => 'Hallo, mijn naam is',
+    'location_label'                => 'Ik kom uit',
+    'no_location'                   => 'Ergens in de Matrix 🌌',
+    'no_company'                    => 'Geen opgave',
+    'created'                       => 'Aangemaakt',
+    'followers'                     => 'Volgers',
+    'following'                     => 'Volg ik',
+    'stars_give'                    => 'Gegeven sterren',
+    'repos'                         => 'Openbare Repositories',
+    'register_on'                   => 'Geregistreerd op',
+    'register_since'                => 'Geregistreerd sinds',
+
+    // Commits
+    'last_commit'                   => 'Laatste Commit',
+
+    // Mijlpalen
+    'milestones_info'               => 'Mijlpalen Info',
+    'milestones_info_open'          => 'Open Mijlpalen (Info)',
+    'milestones_info_closed'        => 'Gesloten Mijlpalen (Info)',
+    'milestones_all_open'           => 'Mijlpalen (Alle Open)',
+    'milestones_all_closed'         => 'Mijlpalen (Alle Gesloten)',
+
+    // Downloads
+    'downloads'                     => 'Downloads',
+    'downloads_latest'              => 'Downloads (Laatste Release)',
+    'downloads_all'                 => 'Downloads (Alle)',
+
+    // Extra labels voor “all”-totalen
+    'stars_all'                     => 'Sterren (Alle)',
+    'watchers_all'                  => 'Volgers (Alle)',
+    'forks_all'                     => 'Forks (Alle)',
+    'size_all'                      => 'Grootte (Alle)',
+    'releases_all'                  => 'Releases (Alle)',
+];
