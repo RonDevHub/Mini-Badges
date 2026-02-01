de854ccca2465cf2e43621cb8beb9b82afe6319c
RonDevHub <no_reply@rondev.de>
2026-02-01 08:56:56 +0000

    lang/bay2.php hinzugefügt
    BAYRISCH‑HARDCORE VERSION

---
+++ lang/bay2.php
@@ -0,0 +1,187 @@
+<?php
+return [
+    // GitHub
+    // Allgemein
+    'stars'                     => 'Stoinerl',
+    'forks'                     => 'Gabelzeig',
+    'issues'                    => 'Gschmarri',
+    'watchers'                  => 'Gaffar',
+    'release'                   => 'Auslasserl',
+    'license'                   => 'Lizenz',
+    'top_language'              => 'Hauptsproch',
+
+    // Metriken (Alle Repos)
+    'stars_all'                 => 'Stoinerl (Ois zamm)',
+    'forks_all'                 => 'Gabelzeig (Ois zamm)',
+    'issues_all'                => 'Gschmarri (Ois zamm)',
+    'watchers_all'              => 'Gaffar (Ois zamm)',
+    'downloads_all'             => 'O’glaadne (Ois zamm)',
+    'branches_all'              => 'Zweigerl (Ois zamm)',
+    'size_all'                  => 'G’samtgrößn',
+    'top_languages_all'         => 'Hauptsprochn (Ois zamm)',
+    'pull_requests_all'         => 'Ziagn-Anfrogn (Ois zamm)',
+    'prs_merged_all'            => 'Zamgschmissene PRs',
+    'subscribers_count_all'     => 'Zuhörerschaft (Ois zamm)',
+    'success_rate_all'          => 'Erfolgsquote (Ois zamm)',
+    'files_all'                 => 'Dateierl (Ois zamm)',
+    'tags_all'                  => 'Bapperl (Ois zamm)',
+    'projects_all'              => 'Gschäftla (Ois zamm)',
+    'releases_all'              => 'Auslasserl (Ois zamm)',
+
+    // Metriken (Einzelnes Repo)
+    'lines'                     => 'Zeiln',
+    'lines_added'               => 'Dazuagkritzlt',
+    'lines_deleted'             => 'Weggradiert',
+    'milestones'                => 'Meilenstoa',
+    'milestones_open'           => 'Offene Meilenstoa',
+    'milestones_closed'         => 'Zugemachte Meilenstoa',
+    'milestones_all'            => 'Meilenstoa (Ois)',
+    'milestones_allopen'        => 'Meilenstoa (Ois offen)',
+    'milestones_allclosed'      => 'Meilenstoa (Ois zua)',
+    'downloads'                 => 'O’glaadne',
+    'downloads_latest'          => 'O’glaadne (Letzts Auslasserl)',
+    'branches'                  => 'Zweigerl',
+    'size'                      => 'G’samtgrößn',
+    'created_at'                => 'Zamgschustert am',
+    'pull_requests'             => 'Ziagn-Anfrogn',
+    'prs_merged'                => 'Zamgschmissene PRs',
+    'subscribers_count'         => 'Zuhörerschaft',
+    'success_rate'              => 'Erfolgsquote',
+    'files'                     => 'Dateierl',
+    'tags'                      => 'Bapperl',
+    'projects'                  => 'Gschäftla',
+    'releases'                  => 'Auslasserl',
+
+    // Push & Commit
+    'push_date'                 => 'Letzda Schubser',
+    'push_time'                 => 'Letzda Schubser',
+    'push_datetime'             => 'Letzda Schubser',
+    'push_lines'                => 'Zeiln',
+    'push_info'                 => 'Gschicht vom letztn Schubser',
+    'commit_date'               => 'Letzda Eitrag',
+    'commit_time'               => 'Letzda Eitrag',
+    'commit_datetime'           => 'Letzda Eitrag',
+    'commit_lines'              => 'Eitrag-Zeiln',
+    'commit_info'               => 'Gschicht vom letztn Eitrag',
+
+    // Benutzer-Metriken
+    'repos_count'               => 'Öffentliche Ablag’n',
+    'follower'                  => 'De wo da hinterherlatschn',
+    'follower_name'             => 'Zuletzt hinterherglatscht',
+    'following'                 => 'De wo du hinterherlatscht',
+    'following_name'            => 'Zuletzt hintnachganga',
+
+    // Gists
+    'gists'                     => 'Gschmarri-Schnipsl',
+    'gist_not_found'            => 'Des Gschmarri gibts ned',
+    'gist_size'                 => 'Gschmarri-Größn',
+    'gist_forks'                => 'Gabelzeig vom Gschmarri',
+    'gists_list_short'          => 'Letzte Gschmarri',
+    'gists_listall'             => 'Ois Gschmarri',
+    'gists_size'                => 'G’samtgrößn vom Gschmarri',
+    'gists_date'                => 'Letzts Gschmarri',
+    'gists_forks'               => 'Gabelzeig',
+
+    // Versionsprüfung
+    'version_check'             => 'Version nachschaugn',
+    'no_version_found'          => 'Koane Version oagebn',
+    'new_release_available'     => 'A neis Auslasserl is do:',
+    'check_release'             => 'Auslasserl nachschaugn',
+    'check_ok'                  => 'Basst scho',
+    'error'                     => 'Owei, a Fehler',
+
+    // Benutzer-Infos (Cases)
+    'name_label'                => 'Griaß di, i bin da',
+    'company_label'             => 'Buidlbud’n',
+    'no_company'                => 'Koan Schmarrn eigtragen',
+    'location_label'            => 'I kimm aus',
+    'no_location'               => 'Irgendwo im boarischen Nirwana 🌌',
+    'status_label'              => 'Zustand',
+    'no_status'                 => 'Nix eigstellt, wia immer',
+    'account_created'           => 'Konto zsammgschustert',
+    'last_updated'              => 'Zuletzt drangschraubt',
+    'time_year_singular'        => 'Jahrerl',
+    'time_year_plural'          => 'Jahrerl',
+    'time_month_singular'       => 'Monaterl',
+    'time_month_plural'         => 'Monaterl',
+    'time_day_singular'         => 'Dog',
+    'time_day_plural'           => 'Dog',
+
+    // Commit/Push Detail-Felder (Cases)
+    'commits_repo'              => 'Eiträg',
+    'commits_all'               => 'Eiträg (Ois)',
+    'commits_last'              => 'Letzda Eitrag (Datum)',
+    'commits_last_info'         => 'Letzda Eitrag (Gschicht)',
+
+    // Diskussionen (Cases)
+    'discussions_count'         => 'Gred',
+    'discussions_lastdate'      => 'Letzts Gred (Datum)',
+    'discussions_lastupdate'    => 'Letzts Gred (Update)',
+    'discussions_lasttitle'     => 'Letzda Gred-Titel',
+    'discussions_lastauthor'    => 'Letzda Gred-Schreiber',
+
+    // Codeberg
+    // Allgemein / Basics
+    'name'                          => 'Nama',
+    'language'                      => 'Sproch',
+    'updated_at'                    => 'Zuletzt drangschraubt',
+    'branch_default'                => 'Standard-Zweigle',
+
+    // Issues (Einzel + Alle Repos)
+    'issues_open'                   => 'Offene Gschmarri',
+    'issues_closed'                 => 'Zua Gschmarri',
+    'issues_total'                  => 'Ois Gschmarri',
+    'issues_allopen'                => 'Offene Gschmarri (Ois)',
+    'issues_all'                    => 'Gschmarri (Ois)',
+    'issues_allclosed'              => 'Zua Gschmarri (Ois)',
+
+    // Pull Requests (Einzel + Alle Repos)
+    'prs_total'                     => 'Ois Ziagn-Anfrogn',
+    'prs_open'                      => 'Offene Ziagn-Anfrogn',
+    'prs_closed'                    => 'Zua Ziagn-Anfrogn',
+    'prs_all'                       => 'Ziagn-Anfrogn (Ois)',
+    'prs_allopen'                   => 'Offene Ziagn-Anfrogn (Ois)',
+    'prs_allclosed'                 => 'Zua Ziagn-Anfrogn (Ois)',
+
+    // Releases & Versionen
+    'release_label'                 => 'Letzts Auslasserl',
+    'release_tag_label'             => 'Neisdes Bapperl',
+    'release_check_label'           => 'Auslasserl-Nachschau',
+    'release_new_label'             => 'Neis Auslasserl do',
+    'releases_label'                => 'Auslasserl g’samt',
+
+    // Benutzer / User
+    'name_label'                    => 'Griaß di, i bin da',
+    'location_label'                => 'I kimm aus',
+    'no_location'                   => 'Irgendwo im boarischen Nirwana 🌌',
+    'no_company'                    => 'Koan Schmarrn eigtragen',
+    'created'                       => 'Zamgschustert',
+    'followers'                     => 'Hinterherlatscher',
+    'following'                     => 'Hintnachganga',
+    'stars_give'                    => 'Vergebene Stoinerl',
+    'repos'                         => 'Öffentliche Ablag’n',
+    'register_on'                   => 'O’gmeldt am',
+    'register_since'                => 'O’gmeldt seit',
+
+    // Commits
+    'last_commit'                   => 'Letzda Eitrag',
+
+    // Milestones
+    'milestones_info'               => 'Meilenstoa-Gschicht',
+    'milestones_info_open'          => 'Offene Meilenstoa (Gschicht)',
+    'milestones_info_closed'        => 'Zua Meilenstoa (Gschicht)',
+    'milestones_all_open'           => 'Meilenstoa (Ois offen)',
+    'milestones_all_closed'         => 'Meilenstoa (Ois zua)',
+
+    // Downloads
+    'downloads'                     => 'O’glaadne',
+    'downloads_latest'              => 'O’glaadne (Letzts Auslasserl)',
+    'downloads_all'                 => 'O’glaadne (Ois)',
+
+    // Zusätzliche Labels für „all“-Summen
+    'stars_all'                     => 'Stoinerl (Ois)',
+    'watchers_all'                  => 'Gaffar (Ois)',
+    'forks_all'                     => 'Gabelzeig (Ois)',
+    'size_all'                      => 'G’samtgrößn (Ois)',
+    'releases_all'                  => 'Auslasserl (Ois)',
+];
